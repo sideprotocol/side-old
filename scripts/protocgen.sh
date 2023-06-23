c04b498ba86d5555a5f803fe80c6a9d41c73b272
DedicatedDev <topdev323@gmail.com>
2023-06-23 20:48:18 +0300

    chore: fix docker image build image path

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -12,7 +12,7 @@ for dir in $proto_dirs; do
   proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
   for file in $proto_files; do
     # Check if the go_package in the file is pointing to cascadia
-    if grep -q "option go_package.*cascadia" "$file"; then
+    if grep -q "option go_package.*sidechain" "$file"; then
       buf generate --template proto/buf.gen.gogo.yaml "$file"
     fi
   done
