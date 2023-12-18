9880544b1a88d6c8ad16773106b5097db8670727
Amit Yadav <amy29981@gmail.com>
2023-12-18 20:17:12 +0530

    add get chain id function

--- x/yield/keeper/keeper.go
+++ x/yield/keeper/keeper.go
@@ -8,6 +8,10 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+	ibckeeper "github.com/cosmos/ibc-go/v7/modules/core/keeper"
+	ibctmtypes "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
+	ibclocalhosttypes "github.com/cosmos/ibc-go/v7/modules/light-clients/09-localhost"
 
 	"github.com/sideprotocol/side/x/yield/types"
 )
@@ -20,6 +24,7 @@ type (
 		paramstore paramtypes.Subspace
 
 		bankKeeper types.BankKeeper
+		ibcKeeper  *ibckeeper.Keeper
 	}
 )
 
@@ -30,6 +35,7 @@ func NewKeeper(
 	ps paramtypes.Subspace,
 
 	bankKeeper types.BankKeeper,
+	ibcKeeper *ibckeeper.Keeper,
 ) *Keeper {
 	// set KeyTable if it has not already been set
 	if !ps.HasKeyTable() {
@@ -43,9 +49,42 @@ func NewKeeper(
 		paramstore: ps,
 
 		bankKeeper: bankKeeper,
+		ibcKeeper:  ibcKeeper,
 	}
 }
 
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+// GetClientState retrieves the client state given a connection id
+func (k *Keeper) GetClientState(ctx sdk.Context, connectionID string) (exported.ClientState, error) {
+	conn, found := k.ibcKeeper.ConnectionKeeper.GetConnection(ctx, connectionID)
+	if !found {
+		return nil, fmt.Errorf("invalid connection id, \"%s\" not found", connectionID)
+	}
+
+	clientState, found := k.ibcKeeper.ClientKeeper.GetClientState(ctx, conn.ClientId)
+	if !found {
+		return nil, fmt.Errorf("client id \"%s\" not found for connection \"%s\"", conn.ClientId, connectionID)
+	}
+
+	return clientState, nil
+}
+
+// GetChainID gets the id of the host chain given a connection id
+func (k *Keeper) GetChainID(ctx sdk.Context, connectionID string) (string, error) {
+	clientState, err := k.GetClientState(ctx, connectionID)
+	if err != nil {
+		return "", fmt.Errorf("client state not found for connection \"%s\": \"%s\"", connectionID, err.Error())
+	}
+
+	switch clientType := clientState.(type) {
+	case *ibctmtypes.ClientState:
+		return clientType.ChainId, nil
+	case *ibclocalhosttypes.ClientState:
+		return ctx.ChainID(), nil
+	default:
+		return "", fmt.Errorf("unexpected type of client, cannot determine chain-id: clientType: %s, connectionid: %s", clientState.ClientType(), connectionID)
+	}
 }
--- x/yield/keeper/msg_server_register_host_chain.go
+++ x/yield/keeper/msg_server_register_host_chain.go
@@ -2,8 +2,11 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/sideprotocol/side/x/yield/types"
 )
 
@@ -11,6 +14,15 @@ func (k msgServer) RegisterHostChain(goCtx context.Context, msg *types.MsgRegist
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
+	if msg.Creator != k.GetParams(ctx).Admin {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "This operation can only be performed by admin")
+	}
+
+	// get the host chain id
+	chainID, err := k.GetChainID(ctx, msg.ConnectionId)
+	if err != nil {
+		return nil, fmt.Errorf("chain id not found for connection \"%s\": \"%w\"", msg.ConnectionId, err)
+	}
 	_ = ctx
 
 	return &types.MsgRegisterHostChainResponse{}, nil
