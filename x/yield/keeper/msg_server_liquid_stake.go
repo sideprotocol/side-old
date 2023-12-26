f6d0bb862639c0d146c66a8b83e1c3e8e109f9dc
Amit Yadav <amy29981@gmail.com>
2023-12-26 23:58:02 +0530

    add receiver

--- x/yield/keeper/msg_server_liquid_stake.go
+++ x/yield/keeper/msg_server_liquid_stake.go
@@ -11,6 +11,12 @@ func (k msgServer) LiquidStake(goCtx context.Context, msg *types.MsgLiquidStake)
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
+	// - Check balance
+	// - Transfer amount given in message
+	// - Deduce host denom from ibc from (msg.denom)
+	// - Check for host zone using hose denom
+	// - Add deposit record with status, amount and id
+	// - Transfer native tokens(1st step of process)
 	_ = ctx
 
 	return &types.MsgLiquidStakeResponse{}, nil
