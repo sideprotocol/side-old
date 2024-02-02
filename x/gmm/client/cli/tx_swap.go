152b8dbdef43f52ab39eb11024f106ce1ea9e915
DedicatedDev <topdev323@gmail.com>
2024-02-02 12:20:22 +0200

    chore: fix gmm swap cli

--- x/gmm/client/cli/tx_swap.go
+++ x/gmm/client/cli/tx_swap.go
@@ -1,8 +1,10 @@
 package cli
 
 import (
+	"fmt"
 	"strconv"
 
+	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
@@ -15,9 +17,9 @@ var _ = strconv.Itoa(0)
 
 func CmdSwap() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "swap [pool_id] [tokenIn] [tokenOut]",
+		Use:   "swap [pool_id] [tokenIn] [tokenOut] [slippage]",
 		Short: "Broadcast message swap",
-		Args:  cobra.ExactArgs(0),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -32,11 +34,22 @@ func CmdSwap() *cobra.Command {
 			if err != nil {
 				return err
 			}
+
+			slippage := sdkmath.NewUintFromString(args[3])
+			// Define the range limits
+			var minSlippage, maxSlippage = sdkmath.NewUint(0), sdkmath.NewUint(100)
+
+			// Check if slippage is within the range [0, 10000]
+			if slippage.LT(minSlippage) || slippage.GT(maxSlippage) {
+				return fmt.Errorf("slippage %d is out of range [0-10000]", slippage)
+			}
+
 			msg := types.NewMsgSwap(
 				clientCtx.GetFromAddress().String(),
 				args[0],
 				tokenIn,
 				tokenOut,
+				sdkmath.Int(slippage),
 			)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
