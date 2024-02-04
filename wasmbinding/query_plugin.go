e86f1ea9202d62dfa069c7f6466160d7c6bc48c4
Amit Yadav <amy29981@gmail.com>
2024-02-04 16:24:30 +0530

    fix

--- wasmbinding/query_plugin.go
+++ wasmbinding/query_plugin.go
@@ -72,8 +72,7 @@ func ConvertSdkCoinsToWasmCoins(coins []sdk.Coin) wasmvmtypes.Coins {
 // ConvertSdkCoinToWasmCoin converts a sdk type coin to a wasm vm type coin
 func ConvertSdkCoinToWasmCoin(coin sdk.Coin) wasmvmtypes.Coin {
 	return wasmvmtypes.Coin{
-		Denom: coin.Denom,
-		// Note: gamm tokens have 18 decimal places, so 10^22 is common, no longer in u64 range
+		Denom:  coin.Denom,
 		Amount: coin.Amount.String(),
 	}
 }
