a6260b7e535736723bc9198e97d151bd0ccf1c40
DedicatedDev <topdev323@gmail.com>
2024-03-07 12:43:31 +0200

    chore: fix proto-gen issue

--- bitcoin/keys/segwit/segwit_test.go
+++ bitcoin/keys/segwit/segwit_test.go
@@ -1,28 +1,27 @@
 package segwit
 
 import (
-	"fmt"
+	//"fmt"
 	"testing"
-
-	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	"github.com/cosmos/cosmos-sdk/types/bech32"
+	// "github.com/cosmos/cosmos-sdk/crypto/hd"
+	// "github.com/cosmos/cosmos-sdk/types/bech32"
+	// "github.com/cosmos/go-bip39"
 	// "github.com/sideprotocol/side/bitcoin/keys/segwit" // Import the segwit package
 )
 
 func TestSegwit(t *testing.T) {
 
+	// seed, _ := bip39.MnemonicToByteArray(mnemonic)
+	// hd.ComputeMastersFromSeed(seed)
 
-	seed, _ := bip39.MnemonicToByteArray(mnemonic)
-	hd.ComputeMastersFromSeed(seed)
+	// parm, err := hd.NewParamsFromPath("m/84'/0'/0'/0/0")
 
-	parm, err := hd.NewParamsFromPath("m/84'/0'/0'/0/0");
+	// privKey := PrivKey{key: ""}
+	// pubKey := privKey.PubKey()
 
-	privKey := PrivKey{	key: }
-	pubKey := privKey.PubKey()
+	// fmt.Println("Address:", pubKey.Address())
+	// t.Log("Address:", pubKey.Address())
 
-	fmt.Println("Address:", pubKey.Address())
-	t.Log("Address:", pubKey.Address())
-
-	addr, _ := bech32.ConvertAndEncode("bc", pubKey.Address().Bytes())
-	t.Log("Bech Address:", addr)
+	// addr, _ := bech32.ConvertAndEncode("bc", pubKey.Address().Bytes())
+	// t.Log("Bech Address:", addr)
 }
