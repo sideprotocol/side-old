e202a9d28fb31988f8a767a884d3cab245fecca3
BitcoinVM <18786721@qq.com>
2024-03-08 19:04:32 +0800

    add encoding test

--- bitcoin/keys/segwit/segwit_test.go
+++ bitcoin/keys/segwit/segwit_test.go
@@ -8,9 +8,9 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/sideprotocol/side/bitcoin/keys/segwit"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/btcutil/bech32"
+	b32 "github.com/cosmos/cosmos-sdk/types/bech32"
 	"github.com/cosmos/go-bip39"
 )
 
@@ -32,8 +32,31 @@ func TestSegwit(t *testing.T) {
 	// Check if the Bech32 encoded address has the correct prefix and structure.
 	assert.True(t, strings.HasPrefix(bech32Address, "bc1q"), "Address should start with 'bc1q'")
 	t.Logf("Generated SegWit Address: %s", bech32Address)
-	acc, err := sdk.AccAddressFromBech32(bech32Address)
-	require.NoError(t, err)
-	t.Logf("Generated SegWit Address: %s", acc)
+
+	// data, err := sdk.GetFromBech32(bech32Address, "bc")
+
+	hrp, version, data, err2 := bech32.DecodeUnsafe(bech32Address)
+	assert.NoError(t, err2)
+
+	println(hrp, version, data)
+	t.Log(hrp)
+
+	hrp, bz, err := bech32.Decode(bech32Address, 1000)
+	assert.NoError(t, err)
+	println(hrp, bz)
+
+	data, er := bech32.ConvertBits(bz, 5, 8, true)
+	assert.NoError(t, er)
+
+	newAddr, er0 := b32.ConvertAndEncode(hrp, data)
+	assert.NoError(t, er0)
+	println(newAddr)
+
+	hrp2, bz2, err3 := b32.DecodeAndConvert(bech32Address)
+	assert.NoError(t, err3)
+	println(hrp2, bz2)
+	// acc, err := sdk.AccAddressFromBech32(bech32Address)
+	// require.NoError(t, err)
+	// t.Logf("Generated SegWit Address: %s", acc)
 
 }
